6de3016dce0902d8b58088c100f84323932cfc0a
Sam Pfeiffer <sam.pfeiffer@pal-robotics.com>
2016-06-07 16:50:25 +0200

    Comment out test code

--- scripts/learn_server.py
+++ scripts/learn_server.py
@@ -64,19 +64,19 @@ if __name__ == '__main__':
     ls = LearnServer()
     rospy.spin()
 
-    jsr = JointStateRecorder()
-    jsr.start("arm_test_motion")
-    rospy.sleep(10.0)
-    bag_name = jsr.stop()
+    # jsr = JointStateRecorder()
+    # jsr.start("arm_test_motion")
+    # rospy.sleep(10.0)
+    # bag_name = jsr.stop()
 
-    rospy.loginfo("Generating...")
-    joints = []
-    for i in range(1, 8):
-        joints.append("arm_" + str(i) + "_joint")
+    # rospy.loginfo("Generating...")
+    # joints = []
+    # for i in range(1, 8):
+    #     joints.append("arm_" + str(i) + "_joint")
 
-    mg = MotionGenerator(bag_name, joints)
+    # mg = MotionGenerator(bag_name, joints)
 
-    mg.generatePlayMotion("LBD_HALFX", play_speed=0.5)
-    mg.generatePlayMotion("LBD_1X", play_speed=1.0)
-    mg.generatePlayMotion("LBD_2X", play_speed=2.0)
-    mg.generatePlayMotion("LBD_3X", play_speed=3.0)
+    # mg.generatePlayMotion("LBD_HALFX", play_speed=0.5)
+    # mg.generatePlayMotion("LBD_1X", play_speed=1.0)
+    # mg.generatePlayMotion("LBD_2X", play_speed=2.0)
+    # mg.generatePlayMotion("LBD_3X", play_speed=3.0)
