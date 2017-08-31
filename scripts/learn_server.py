d52bcc26d8cc1a1460e10bc1299078b8b8967316
Jordi Pages <jordi.pages@pal-robotics.com>
2017-08-31 10:31:28 +0200

    add waypoint mode

--- scripts/learn_server.py
+++ scripts/learn_server.py
@@ -2,8 +2,8 @@
 """
 Created on 06/05/16
 
-@author: Sammy Pfeiffer
-@email: sam.pfeiffer@pal-robotics.com
+@author: Sammy Pfeiffer, Jordi Pages
+@email: {sam.pfeiffer, jordi.pages}@pal-robotics.com
 
 """
 
@@ -20,16 +20,20 @@ class LearnServer(object):
         self.learning = False
         self.bag_name = "/tmp/" + "lbd_motion"
         self.last_joints_to_learn = None
+        self.recording_individual_waypoints = False
+        self.seconds_for_each_waypoint = 0 # only used when recording individual waypoints
         self.start_srv = rospy.Service('/learn_by_demo_start',
                                        Learn,
                                        self.start_cb)
         self.stop_srv = rospy.Service('/learn_by_demo_stop',
                                       Learn,
-                                      self.stop_cb)
+                                      self.stop_cb)                                      
         rospy.loginfo("Learning by demo service ready.")
 
     def start_cb(self, req):
-        self.jsr.start(self.bag_name)
+        self.recording_individual_waypoints = req.record_single_waypoint
+        self.seconds_for_each_waypoint = req.seconds_for_each_waypoint
+        self.jsr.start(self.bag_name, req.record_single_waypoint)
         self.last_joints_to_learn = req.joints_to_learn
         rospy.loginfo("Will learn from joints: " + str(req.joints_to_learn))
         self.learning = True
@@ -50,7 +54,11 @@ class LearnServer(object):
             else:
                 joints = req.joints_to_learn
         print "Stop cb, joints to learn:" + str(joints)
-        mg = MotionGenerator(self.bag_name + ".bag", joints)
+        frequency_to_downsample = 10
+        mg = MotionGenerator(self.bag_name + ".bag", joints,
+                             individual_waypoints_recorded = self.recording_individual_waypoints,
+                             seconds_for_each_waypoint = self.seconds_for_each_waypoint,
+                             frequency_to_downsample = 10)
         mg.generatePlayMotion("LBD_HALFX", play_speed=0.5)
         motion_dict = mg.generatePlayMotion("LBD_1X", play_speed=1.0)
         mg.generatePlayMotion("LBD_2X", play_speed=2.0)
@@ -63,20 +71,3 @@ if __name__ == '__main__':
     rospy.init_node('learning_by_demo_server')
     ls = LearnServer()
     rospy.spin()
-
-    # jsr = JointStateRecorder()
-    # jsr.start("arm_test_motion")
-    # rospy.sleep(10.0)
-    # bag_name = jsr.stop()
-
-    # rospy.loginfo("Generating...")
-    # joints = []
-    # for i in range(1, 8):
-    #     joints.append("arm_" + str(i) + "_joint")
-
-    # mg = MotionGenerator(bag_name, joints)
-
-    # mg.generatePlayMotion("LBD_HALFX", play_speed=0.5)
-    # mg.generatePlayMotion("LBD_1X", play_speed=1.0)
-    # mg.generatePlayMotion("LBD_2X", play_speed=2.0)
-    # mg.generatePlayMotion("LBD_3X", play_speed=3.0)
